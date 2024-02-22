0e425a21c4b6ef08a78c9655f72085c7d5e63d58
Vlad Sunny <vladislavsunny@gmail.com>
2024-02-22 12:27:09 +0300

    API

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,38 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	// Импортируем на типы и функционал Svelte
+	import { onMount } from 'svelte';
+  
+	// Пример типа данных, которые ожидаются от API
+	interface ApiResponse {
+	  message: string;
+	}
+  
+	// Переменная для хранения полученных данных
+	let data: ApiResponse[] | null = null;
+  
+	// Переменная для хранения возможной ошибки
+	let error: string | null = null;
+  
+	// Функция для выполнения запроса к API
+	async function fetchData() {
+	  try {
+		const response = await fetch('https://mlstudy.pythonanywhere.com/hello/?name=Fedos');
+		if (!response.ok) {
+		  throw new Error('Ошибка запроса: ' + response.statusText);
+		}
+		const jsonData: ApiResponse[] = await response.json();
+		data = jsonData;
+		console.log(data.message);
+	  } catch (err) {
+		error = err.message;
+	  }
+	}
+  
+	// Выполняем запрос при монтировании компонента
+	onMount(() => {
+	  fetchData();
+	});
+  </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
@@ -12,4 +46,5 @@
 			</li>
 		</ul>
 	</div>
+	<h1>{data?.message}</h1>
 </div>
